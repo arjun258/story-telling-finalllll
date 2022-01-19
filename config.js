0171ca599cf6928d99c97284ee390de4cb2e1cb1
arjun258 <66076537+arjun258@users.noreply.github.com>
2022-01-19 21:35:36 +0530

    final

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD8BumlUhsEF0hSIYRTGDQKQ9yBOKdVGPE",
+    authDomain: "story-telling-2f591.firebaseapp.com",
+    databaseURL: "https://story-telling-2f591-default-rtdb.firebaseio.com",
+    projectId: "story-telling-2f591",
+    storageBucket: "story-telling-2f591.appspot.com",
+    messagingSenderId: "497417895091",
+    appId: "1:497417895091:web:8313b7352b79b8a25204c6"
+  };
